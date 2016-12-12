16557531eb18b834335f0733de8be926bde64124
yingchenpeng <ychp@terminus.io>
2016-12-12 17:49:01 +0800

    add shiro realm

--- auth/src/main/java/com/ychp/club/auth/infrastructure/impl/realm/CustomerShiroRealm.java
+++ auth/src/main/java/com/ychp/club/auth/infrastructure/impl/realm/CustomerShiroRealm.java
@@ -1,13 +1,17 @@
 package com.ychp.club.auth.infrastructure.impl.realm;
 
+import com.google.common.collect.Lists;
 import com.ychp.club.user.application.UserManager;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.ychp.club.user.model.Role;
+import com.ychp.club.user.model.User;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * Desc:
@@ -25,11 +29,34 @@ public class CustomerShiroRealm extends AuthorizingRealm {
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String username = (String) super.getAvailablePrincipal(principalCollection);
+        User user = userManager.findByUsername(username);
+        if(user != null){
+            Role role = new Role();
+            role.setName("admin");
+
+            SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+            simpleAuthorizationInfo.addRole(role.getName());
+
+            List<String> permissions = Lists.newArrayList();
+
+            simpleAuthorizationInfo.addStringPermissions(permissions);
+            return simpleAuthorizationInfo;
+        }
+
         return null;
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+
+        User user = userManager.findByUsername(token.getUsername());
+
+        if(user != null){
+            return new SimpleAuthenticationInfo(user.getName(), user.getPassword(), getName());
+        }
+
         return null;
     }
 }
